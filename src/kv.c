6549c914f4ba58947174678360e1c4559dd3b5ba
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-07-06 15:45:37 -0400

    added put function

--- src/kv.c
+++ src/kv.c
@@ -1,4 +1,8 @@
 #include <kv.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define TOMBSTONE 0x1
 
 kv_t *kv_init(size_t capacity) {
 
@@ -21,4 +25,75 @@ kv_t *kv_init(size_t capacity) {
 
     return table;
 }
+
+size_t hash(char *value, int capacity) {
+    size_t hash = 0x13371337deadbeef;
+
+    while(*value) {
+        hash ^= *value;
+        hash = hash << 8;
+        hash += *value;
+
+        value++;
+    }
+
+    return hash % capacity;
+}
+
+//kv-put
+//params:
+//  db : pointer to the db
+//  key : pointer to the key val
+//  value : ponter to value itself
+//returns: index of the key, otherwise -1 on err and -2 not found
+int kv_put(kv_t *db, char *key, char *value) {
+    if (!db || !key || !value) {
+        return -1;
+    }
+
+    size_t idx = hash(key, db->capacity);
+
+    for(int i = 0; i < db->capacity -1; i++) {
+
+        size_t real_idx = (idx + 1) % db->capacity;
+        
+        kv_entry_t *entry = &db->entries[real_idx];
+
+        // key is already set, updating
+        if (entry->key && entry->key != (void*)TOMBSTONE
+            && !strcmp(entry->key,key)) {
+            
+            char *newval = strdup(value);
+            if (!newval) {
+                return -1;
+            }
+            entry->value = newval;
+            return real_idx;
+        }
+
+        //land in a slot that is empty
+        //null or tombstone
+        if(!entry -> key || entry->key == (void*)TOMBSTONE) {
+            char *newval = strdup(value);
+            char *newkey = strdup(key);
+
+            if (!newval || !newkey) {
+                free(newkey);
+                free(newval);
+                
+                return -1;
+            } 
+
+            entry->value = newval;
+            entry->key = newkey;
+            db->count++;
+            return real_idx;
+        }
+    }
+
+    //db is occupied
+    return -1;
+}
+
+
 
--- src/main.c
+++ src/main.c
@@ -3,6 +3,13 @@
 
 int main() {
      kv_t *table = kv_init(3);
-    printf("%p", table);
+    printf("%p\n", table);
 
+    kv_put(table, "hehe", "haha");
+    
+    for(int i = 0; i < table->capacity; i++) {
+        if(table->entries[i].key) {
+            printf("%s: %s\n", table->entries[i].key, table->entries[i].value);
+        }
+    }
 }
